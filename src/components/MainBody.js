35df037528b37b7926411c1d31369572dd242be0
Dan Brioli <technicaldebtgames@gmail.com>
2020-10-08 02:40:04 -0400

    :lipstick: Added header text for content

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -9,10 +9,11 @@ const MainBody = (props) => {
 
     return (
         <div className='MainBody'>
+            <h1>Projects</h1>
             <h2><a href='https://thevarietyshow.netlify.app/'>The Dan and Liz Variety Show</a></h2>
             <h2><a href='https://stylex.netlify.app/'>STYLEX</a></h2>
             <h2><a href='https://video-essays.netlify.app/'>Video Essay Transcripts</a></h2>
-            <h2>Art</h2>
+            <h1>Art</h1>
             <img className='art-svgs' src={AngryFace} alt='Angry Face'/>
             <img className='art-svgs' src={DesertTrees} alt='Desert Trees'/>
             <p>(you've reached the end of the internet)</p>
