fa249e2468e4e9976653ff97162fc8b253430e9d
Dan Brioli <technicaldebtgames@gmail.com>
2020-10-08 06:36:51 -0400

    :bento: :lipstick: Added monkey paw art, added borders to art

--- src/components/MainBody.js
+++ src/components/MainBody.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 import DesertTrees from '../images/DesertTrees.svg';
 import AngryFace from '../images/angry_face.svg';
+import MonkeyPaw from '../images/MonkeyPaw.svg';
 
 import './MainBody.css';
 
@@ -15,6 +16,7 @@ const MainBody = (props) => {
             <h2><a href='https://video-essays.netlify.app/'>Video Essay Transcripts</a></h2>
             <h1>Art</h1>
             <img className='art-svgs' src={AngryFace} alt='Angry Face'/>
+            <img className='art-svgs' src={MonkeyPaw} alt='Monkey Paw'/>
             <img className='art-svgs' src={DesertTrees} alt='Desert Trees'/>
             <p>(you've reached the end of the internet)</p>
         </div>
