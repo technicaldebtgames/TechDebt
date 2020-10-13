6eabe335275c73009f9de3bd473c26317966cfab
Dan Brioli <technicaldebtgames@gmail.com>
2020-10-13 06:06:49 -0400

    :speech_balloon: Changed social links

--- src/components/Social.js
+++ src/components/Social.js
@@ -15,7 +15,7 @@ const Social = (props) => {
             <a href='mailto:dan.s.brioli@gmail.com'><img src={Envelope} alt='Email'/></a>
             <a href='https://www.linkedin.com/in/dan-brioli/'><img src={LinkedIn} alt='LinkedIn'/></a>
             <a href='https://github.com/technicaldebtgames'><img src={Github} alt='Github'/></a>
-            <a href='https://twitter.com/TechDebtGames'><img src={Twitter} alt='Twitter'/></a>
+            <a href='https://twitter.com/TechDebtWriting'><img src={Twitter} alt='Twitter'/></a>
             <a href='https://www.youtube.com/channel/UCSkWWnCdZMTm7yWU-hLD5UQ'><img src={Youtube} alt='Youtube'/></a>
         </div>
     );
