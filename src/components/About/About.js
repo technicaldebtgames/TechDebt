0b34c1e4126c18db349494f2c98e1f5ae0bbb886
Dan Brioli <technicaldebtgames@gmail.com>
2020-10-22 19:50:03 -0400

    :speech_balloon: Added writing xp to about comp

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -18,6 +18,7 @@ const About = (props) => {
                 <h3>Hi, I'm Dan.</h3>
                 <img className='profile-img' src={ProfileImage} alt='Dan Brioli' />
                 <p>I'm here to make your life easier with <b>content and illustrations</b> for your tech or B2B blog, <b>design and development solutions</b> for your website or brand, and <b>clear and concise documentation</b> for your users.</p>
+                <p>I'm currently writing for <a href='https://medium.com/swlh'>The Startup</a>, Medium's largest publication. In the past, I've written user documentation for <a href='https://www.residentialhealthcaregroup.com/'>Celtic Healthcare</a>.</p>
                 <p><a href='mailto:dan.s.brioli@gmail.com'>Send me an email</a> or visit my social links at the top of the page so we can work together on getting you results!</p>
             </div>
         </div>
