79acfef8185086f1a5bbe5b0b9a3a6c00fc5099c
Dan Brioli <technicaldebtgames@gmail.com>
2021-02-04 13:34:20 -0500

    :speech_balloon: Adjusted text and links

--- src/content/articles/MediumArticles.js
+++ src/content/articles/MediumArticles.js
@@ -5,19 +5,19 @@ const MediumArticles = (props) => {
     return (
         <article>
             <h3>Some Of My Medium Articles</h3>
-            <p>Don't worry, these are Medium friend links. You won't run into any paywalls here.</p>
+            <p>This is a selection of my most popular or my personal favorite articles submitted to various Medium publications.</p>
+            <p>Don't worry, these are 'friend links'. You won't run into any paywalls here.</p>
             <h4>Browse <a href='https://danbrioli.medium.com/'>my Medium profile</a> for more!</h4>
-            <p><a href='https://medium.com/dev-genius/2-best-practices-for-react-file-structures-b07da6c477aa?sk=f4d5ac164658d2cf268c7f6c7130e1f9'>2 Best Practices For React File Structures</a> - Two tips to help manage React file structures.</p>
+            <p><a href='https://medium.com/illumination/the-man-who-saved-the-world-ab898a149391?sk=8efeee287be65dbd550ac7a3c01f4498'>The Man Who Saved the World</a> - The little-known story of Stanislav Petrov, who ignored state-of-the-art automated warnings and prevented nuclear war.</p>
             <p><a href='https://writingcooperative.com/its-ok-to-use-the-passive-voice-in-your-writing-b8d1d43d5006?sk=cc5833079d3500ae2a3a1a5459835053'>It’s Ok to Use the Passive Voice in Your Writing</a> - How and when to properly use it.</p>
+            <p><a href='https://medium.com/dev-genius/2-best-practices-for-react-file-structures-b07da6c477aa?sk=f4d5ac164658d2cf268c7f6c7130e1f9'>2 Best Practices For React File Structures</a> - Two tips to help manage React file structures.</p>
             <p><a href='https://medium.com/dev-genius/the-local-maxima-trap-of-iterative-development-1fde2a529894?sk=7fa31e77cd27ffb471d5819c1fbdf1a7'>The Local Maxima Trap Of Iterative Development</a> - An analogy of a photographer on a mountain, and how local maxima are traps that stop you from reaching your best product version.</p>
             <p><a href='https://medium.com/dev-genius/the-ninety-ninety-rule-and-paretos-principle-97d7c734747f?sk=585bb2e161826933c787b5ab22892dcd'>The Ninety-Ninety Rule And Pareto's Principle</a> - A comparison of a tongue-in-cheek "law" of software development and a more serious law of the distribution of resources.</p>
-            <p><a href='https://medium.com/politically-speaking/my-kingdom-for-an-app-6dce60f00bb3?sk=675c80c12de9c81081ddd116f3af724c'>My Kingdom For An App!</a> - A scathing critique of Donald Trump's unscrupulous business practices in the wake of the Capitol Building invasion, and my prediction for what his next business venture will be.</p>
             <p><a href='https://medium.com/small-business-strong/start-your-deep-work-strategy-today-a8a3a26efaaa?source=friends_link&sk=ee83bf60f8ed2d5faa9c196fa2c0d3f8'>Start Your Deep Work Strategy Today</a> - Key takeaways from Cal Newport's concept of deep work.</p>
-            <p><a href='https://medium.com/illumination/to-all-the-past-me-s-473aeaa6deca?sk=932d9bd7258b4fb30f16dad2a36fa244'>To All The Past "Me"s</a> - In which I give some commentary and advice to various iterations of my past self.</p>
             <p><a href='https://medium.com/burn-rate-zero/lets-make-a-free-art-portfolio-9da157c64def'>Let's Make A Free Art Portfolio!</a> - A quick guide for artists on how to create a visual art portfolio using Github Pages.</p>
             <p><a href='https://medium.com/burn-rate-zero/an-introduction-to-your-food-147ff8acae9e?sk=336999d547f9e04049dd58321bd89013'>An Introduction To Your Food</a> - An examination of our relationship with food and how to start fixing it.</p>
             <p><a href='https://levelup.gitconnected.com/does-technical-debt-exist-714838eaf0f8?source=friends_link&sk=9d6963d9bc31a9c43befe174089b15a7'>Does Technical Debt Exist?</a> - A description of the term, a real-world example of it, and how to reduce its presence in your organization. (This article was localized for InfoQ China, and you can read the Chinese translation <a href='https://www.infoq.cn/article/G9hyluiAvCiAhX11Badv'>here</a>.)</p>
-            <p><a href='https://medium.com/swlh/how-i-ruined-my-bosss-attempt-at-gamification-729261677cbc?source=friends_link&sk=fffdf5b998fa3b11b2acaf84a73b1bb0'>How I Ruined My Boss's Attempt At Gamification</a> - An analysis of a real-world attempt at gamifying the workplace. (This is the article that really boosted my interest in writing professionally - and it sucks!)</p>
+            <p><a href='https://medium.com/swlh/how-i-ruined-my-bosss-attempt-at-gamification-729261677cbc?source=friends_link&sk=fffdf5b998fa3b11b2acaf84a73b1bb0'>How I Ruined My Boss's Attempt At Gamification</a> - An analysis of a real-world attempt at gamifying the workplace. (This is the article that really boosted my interest in developing my writing skills - and it <i>sucks!</i>)</p>
         </article>
     );
 
