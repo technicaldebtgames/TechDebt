911273210a6cf9b4d107482e82eab1e44b42d73c
Dan Brioli <technicaldebtgames@gmail.com>
2021-02-22 13:50:48 -0500

    :lipstick: Social link spacing

--- src/components/Social/Social.js
+++ src/components/Social/Social.js
@@ -14,7 +14,7 @@ const Social = (props) => {
             <a href='mailto:dan.s.brioli@gmail.com'><img src={Envelope} alt='Email'/></a>
             <a href='https://www.linkedin.com/in/dan-brioli/'><img src={LinkedIn} alt='LinkedIn'/></a>
             <a href='https://github.com/technicaldebtgames'><img src={Github} alt='Github'/></a>
-            <a href='https://twitter.com/TechDebtWriting'><img src={Twitter} alt='Twitter'/></a>
+            {/*<a href='https://twitter.com/TechDebtWriting'><img src={Twitter} alt='Twitter'/></a>*/}
         </div>
     );
 
